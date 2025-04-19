ebe5b7fce4d430a2e6344083b613cd7a3fc75526
dennisschumacher <schumacher_dennis@outlook.com>
2025-04-19 20:45:07 +0200

    base view

---
+++ lib/base_view.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:season_planer/services/auth_service.dart';
+import 'package:season_planer/services/database_service.dart';
+
+import 'core/AppState.dart';
+import 'features/authentification/login/login_view.dart';
+
+class BaseView extends StatefulWidget {
+  @override
+  _BaseViewState createState() => _BaseViewState();
+}
+
+class _BaseViewState extends State<BaseView> {
+  bool? _isLoggedIn;
+  bool _isLoadingUserData = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLoginStatus();
+  }
+
+  Future<void> _checkLoginStatus() async {
+    final loggedIn = await AuthService().isLoggedIn();
+    setState(() {
+      _isLoggedIn = loggedIn;
+    });
+
+    if (loggedIn) {
+      _loadUserData();
+    }
+  }
+
+  _loadUserData() async {
+    try {
+      final user = await DatabaseService().getUserInformation();
+      AppState().setUser(user!);
+      Navigator.pushNamed(context, "/home");
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(_isLoggedIn==null){
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    if(_isLoggedIn == false){
+      return LoginView();
+    }
+
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
