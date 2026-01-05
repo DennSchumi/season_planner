154de2770f9fb02432e2e0517c66c6d4591a20e9
DennSchumi <schumacher_dennis@outlook.com>
2026-01-05 16:50:25 +0100

    Navigation back to User Mode

--- lib/features/admin_features/manage_flight_school_view/manage_flight_school_view.dart
+++ lib/features/admin_features/manage_flight_school_view/manage_flight_school_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:season_planer/features/user_features/main_scaffold/main_user_scaffold_view.dart';
 
 class ManageFlightSchoolView extends StatefulWidget{
   const ManageFlightSchoolView({super.key});
@@ -12,7 +13,28 @@ class _ManageFlightSchoolView extends State<ManageFlightSchoolView>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+        body: SafeArea(
+          child:
+        OutlinedButton(
+          onPressed:
+              () => {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MainUserScaffoldView(),
+              ),
+            ),
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("Switch to USER-Mode"),
+              SizedBox(width: 8),
+              Icon(Icons.accessible_forward_outlined),
+            ],
+          ),
+        ),
+        ),
     );
   }
 
