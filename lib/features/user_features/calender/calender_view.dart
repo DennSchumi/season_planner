0498907bc52738d11b848a1523d9c874a4a5f138
DennSchumi <schumacher_dennis@outlook.com>
2026-01-09 12:29:57 +0100

    calender view for Users

--- lib/features/user_features/calender/calender_view.dart
+++ lib/features/user_features/calender/calender_view.dart
@@ -1,4 +1,10 @@
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../services/providers/user_provider.dart';
+import '../../widgets/calender_widget.dart';
+import '../home/widgets/event_detail_view.dart';
 
 class CalenderView extends StatefulWidget{
   const CalenderView({super.key});
@@ -9,11 +15,32 @@ class CalenderView extends StatefulWidget{
 
 class _CalenderView extends State<CalenderView>{
 
+
   @override
   Widget build(BuildContext context){
+    final user = context.watch<UserProvider>().user!;
+    final fsById = { for (final fs in user.flightSchools) fs.id: fs };
+
     return Scaffold(
       body: Center(
-        child: Text("Kalender"),
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                'Kalender',
+                style: TextStyle(fontSize: 26),
+              ),
+            ),
+            SizedBox(height: 16,),
+            EventsCalendar(
+              events: user.events,
+              flightSchoolById: (id) => fsById[id],
+              onEventTap: (e) {
+                Navigator.push(context, MaterialPageRoute(builder: (_) => EventDetailView(event: e)));
+              },
+            )
+          ],
+        ),
       ),
     );
   }
