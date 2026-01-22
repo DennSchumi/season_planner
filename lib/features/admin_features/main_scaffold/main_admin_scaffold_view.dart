868d116d67d73ffdb2dba32abea95e684863edb7
DennSchumi <schumacher_dennis@outlook.com>
2026-01-22 13:44:47 +0100

    implement periodic reload of Data

--- lib/features/admin_features/main_scaffold/main_admin_scaffold_view.dart
+++ lib/features/admin_features/main_scaffold/main_admin_scaffold_view.dart
@@ -1,69 +1,114 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:season_planer/features/admin_features/calender_view/calender_view.dart';
 import 'package:season_planer/features/admin_features/manage_events_view/manage_events_view.dart';
 import 'package:season_planer/features/admin_features/manage_flight_school_view/manage_flight_school_view.dart';
 import 'package:season_planer/features/admin_features/manage_personal/manage_personal_view.dart';
+
 import '../../../data/models/user_models/user_model_userView.dart';
 import '../../../services/database_service.dart';
 import '../../../services/providers/flight_school_provider.dart';
 import '../../../services/providers/user_provider.dart';
 
-class MainAdminScaffoldView extends StatefulWidget{
-   int? selected_index;
+class MainAdminScaffoldView extends StatefulWidget {
+  final int? selected_index;
 
-  MainAdminScaffoldView(
-  {
-   super.key,
-  this.selected_index
-});
+  const MainAdminScaffoldView({
+    super.key,
+    this.selected_index,
+  });
 
   @override
   _MainAdminScaffoldState createState() => _MainAdminScaffoldState();
 }
-class _MainAdminScaffoldState extends State<MainAdminScaffoldView>{
+
+class _MainAdminScaffoldState extends State<MainAdminScaffoldView> {
   int _selectedIndex = 0;
+  Timer? _refreshTimer;
+  bool isLoading = false;
+  bool hasConnection = true;
+  DateTime? lastUpdated;
 
 
-  final List<Widget> _widgetList = <Widget>[
-    ManageEventsView(),
-    ManagePersonalView(),
-    CalenderViewFlightSchool(),
-    ManageFlightSchoolView()
+  List<Widget> get _widgetList => [
+    ManageEventsView(
+      isLoading: isLoading,
+      hasConnection: hasConnection,
+      lastUpdated: lastUpdated,
+    ),
+    ManagePersonalView(
+      isLoading: isLoading,
+      hasConnection: hasConnection,
+      lastUpdated: lastUpdated,),
+    CalenderViewFlightSchool(  isLoading: isLoading,
+      hasConnection: hasConnection,
+      lastUpdated: lastUpdated,),
+    ManageFlightSchoolView(),
   ];
+
 
   @override
   void initState() {
     super.initState();
-    _selectedIndex = widget.selected_index?? 0;
+    _selectedIndex = widget.selected_index ?? 0;
     _loadUser();
+    _startAutoRefresh();
   }
-
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
+
   Future<void> _loadUser() async {
+    setState(() {
+      isLoading = true;
+    });
+
     try {
       final user = await _getUserInformation();
       if (user != null) {
         Provider.of<UserProvider>(context, listen: false).setUser(user);
+        setState(() {
+          hasConnection = true;
+          lastUpdated = DateTime.now();
+        });
       }
     } catch (e) {
-      print('Fehler: $e');
+      debugPrint('Fehler beim Laden des Users: $e');
+      setState(() {
+        hasConnection = false;
+      });
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
     }
   }
 
 
+
   Future<UserModelUserView?> _getUserInformation() {
-  return DatabaseService().getUserInformation();
-}
+    return DatabaseService().getUserInformation();
+  }
+
+  void _startAutoRefresh() {
+    _refreshTimer = Timer.periodic(const Duration(minutes: 1), (_) => _loadUser());
+  }
+
+  @override
+  void dispose() {
+    _refreshTimer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final flightSchoolProvider = Provider.of<FlightSchoolProvider>(context);
     final userProvider = Provider.of<UserProvider>(context).user;
+
     if (userProvider == null) {
       return const Scaffold(
         body: Center(child: CircularProgressIndicator()),
@@ -71,7 +116,7 @@ class _MainAdminScaffoldState extends State<MainAdminScaffoldView>{
     }
 
     return Scaffold(
-      body: Center(child: _widgetList.elementAt(_selectedIndex)),
+      body: _widgetList[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         items: const [
@@ -86,5 +131,4 @@ class _MainAdminScaffoldState extends State<MainAdminScaffoldView>{
       ),
     );
   }
-
 }
