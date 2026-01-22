5e208164f411d6203f024fe42cea0acfdd7f2e81
DennSchumi <schumacher_dennis@outlook.com>
2026-01-22 13:31:16 +0100

    minimize

--- lib/data/enums/event_user_status_enum.dart
+++ lib/data/enums/event_user_status_enum.dart
@@ -1,57 +1,33 @@
 enum EventUserStatusEnum {
-    open,
-    accepted_flight_school,
-    accepted_user,
-    pending_flight_school,
-    pending_user,
-    denied_user,
-    denied_flight_school,
-    user_requests_change,
+  open,
+  accepted_flight_school,
+  accepted_user,
+  pending_flight_school,
+  pending_user,
+  denied_user,
+  denied_flight_school,
+  user_requests_change,
 }
 
 extension EventUserStatusLabel on EventUserStatusEnum {
-    String get label {
-        switch (this) {
-            case EventUserStatusEnum.open:
-                return 'Open Opportunity';
-            case EventUserStatusEnum.accepted_flight_school:
-                return 'Accepted by Flight School';
-            case EventUserStatusEnum.accepted_user:
-                return 'Accepted by You';
-            case EventUserStatusEnum.pending_flight_school:
-                return 'Waiting for Flight School';
-            case EventUserStatusEnum.pending_user:
-                return 'Waiting for Your Response';
-            case EventUserStatusEnum.denied_user:
-                return 'Declined by You';
-            case EventUserStatusEnum.denied_flight_school:
-                return 'Declined by Flight School';
-            case EventUserStatusEnum.user_requests_change:
-                return 'User requeststo Change';
-        }
+  String get label {
+    switch (this) {
+      case EventUserStatusEnum.open:
+        return 'Open Opportunity';
+      case EventUserStatusEnum.accepted_flight_school:
+        return 'Accepted by Flight School';
+      case EventUserStatusEnum.accepted_user:
+        return 'Accepted by You';
+      case EventUserStatusEnum.pending_flight_school:
+        return 'Waiting for Flight School';
+      case EventUserStatusEnum.pending_user:
+        return 'Waiting for Your Response';
+      case EventUserStatusEnum.denied_user:
+        return 'Declined by You';
+      case EventUserStatusEnum.denied_flight_school:
+        return 'Declined by Flight School';
+      case EventUserStatusEnum.user_requests_change:
+        return 'User requests to Change';
     }
+  }
 }
-
-extension EventUserStatusLabelForNewEvent on EventUserStatusEnum {
-    String get label {
-        switch (this) {
-            case EventUserStatusEnum.open:
-                return 'Open Opportunity';
-            case EventUserStatusEnum.accepted_flight_school:
-                return 'Accepted by Flight School';
-            case EventUserStatusEnum.accepted_user:
-                return 'Accepted by User';
-            case EventUserStatusEnum.pending_flight_school:
-                return 'Waiting for Flight School';
-            case EventUserStatusEnum.pending_user:
-                return 'Waiting for User';
-            case EventUserStatusEnum.denied_user:
-                return 'Declined by User';
-            case EventUserStatusEnum.denied_flight_school:
-                return 'Declined by Flight School';
-          case EventUserStatusEnum.user_requests_change:
-            return 'User requests to Change';
-        }
-    }
-}
-
