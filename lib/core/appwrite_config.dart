57dbbb77c9836ea88407b1a20f7d33bbd203509f
DennSchumi <schumacher_dennis@outlook.com>
2026-02-15 21:07:46 +0100

    hm

---
+++ lib/core/appwrite_config.dart
@@ -0,0 +1,12 @@
+import 'dart:core';
+class AppwriteConfig{
+  String appwriteEnpoint ="https://appwrite.schirmschumi.de/v1";
+  String projectId = "693ebfc00021af605014";
+  String mainDatabaseId ="693ebfe20019de61cf09";
+  String usersCollectionID ="693ebfe8002f50b297c3";
+  String flightSchoolsCollectionId = "693ebff40010c03b12ab";
+  String membershipsId = "memberships";
+  String flightSchoolLogosId = "695be45800189fad45f0";
+
+  String flightSchoolFunctionsId = "695a659000312dc45be3";
+}
